499abacc17fc2317888676c9f38f7de946f68623
Brent Jackson <jxnblk@gmail.com>
2019-08-10 12:13:26 -0400

    Adjust layout

--- src/layout.js
+++ src/layout.js
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx, Styled } from 'theme-ui'
+import { Link } from 'gatsby'
 
 export default props => {
   const fullwidth = props.location.pathname === '/'
@@ -11,6 +12,18 @@ export default props => {
         maxWidth: fullwidth ? 'none' : 'container',
         mx: 'auto',
       }}>
+      <header>
+        <Styled.a
+          as={Link}
+          to='/'
+          sx={{
+            fontWeight: 'bold',
+            color: 'inherit',
+            textDecoration: 'none',
+          }}>
+          Notebook
+        </Styled.a>
+      </header>
       {props.children}
     </Styled.root>
   )
